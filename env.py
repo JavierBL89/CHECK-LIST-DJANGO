db46200aa5876da39489f471bbf2d3620916e7bb
Javier <notri80@gmail.com>
2022-03-24 12:28:30 +0100

    deployment commit

--- env.py
+++ env.py
@@ -2,3 +2,4 @@ import os
 
 os.environ["DATABASE_URL"] = "postgres://usdrokfhlgleol:c4d58c16a548784697f440a2cebb65b17f780582e7da61d287670be5903550ca@ec2-63-32-248-14.eu-west-1.compute.amazonaws.com:5432/d1te6egvgag3t1"
 os.environ["SECRET_KEY"] = "django-insecure-8tlk$f)%9h61j%6x+moia!ijh26bsxgv%ot*brqs&qn4xb@jd$"
+os.environ["CLOUDINARY"] = "cloudinary://383432496917636:xO8JmQesM3VSSZCv0zBHoG6tMyw@dfyu20hur"
